2302b60074fb7bdf9d8524330743da78906137bd
aitorcas <aitorcas23@gmail.com>
2024-03-23 13:39:55 +0100

    Error message from python

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
+import os
 
 if __name__ == "__main__":
+    file_name = os.path.basename(__file__)
+    print(f"::error file={file_name}::")
+
     raise ValueError("Error with value error")
     # print("Error message mereketengue")
     # sys.exit(1)
