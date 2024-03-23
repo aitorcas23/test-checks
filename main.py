b75f22bb3e5ab1968b38ce0cf2f26236a9c741c0
aitorcas <aitorcas23@gmail.com>
2024-03-23 14:33:04 +0100

    Can I do it just with print?

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 
 if __name__ == "__main__":
+    print("::warning file=main.py,line=5,title=Warning title::Send warning from python script")
     subprocess.call(
         "echo '::error file=main.py,line=1,title=Where is this title?::Send error from python script'",
         shell=True,
