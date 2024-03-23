5696cb170db3047419daa517eb02fc09a1f30339
aitorcas <aitorcas23@gmail.com>
2024-03-23 23:43:07 +0100

    No change on docker compose

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ if __name__ == "__main__":
     # print("::warning file=main.py,line=5,title=Warning title::Send warning from python script")
     # raise ValueError("Error with value error")
 
-    # Change this file
+    # one last change
     print("Hello github action")
