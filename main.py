01fe642d55c9f97ee95197bb5886f413a50c41c4
aitorcas <aitorcas23@gmail.com>
2024-03-23 13:58:18 +0100

    Error from python again

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import sys
 import subprocess
+import sys
 
 if __name__ == "__main__":
-    subprocess.call("echo 'cantimplora'", shell=True)
+    subprocess.call(
+        "echo '::error file=main.py,line=1,title=Where is this title?::Send error from python script'",
+        shell=True,
+    )
     raise ValueError("Error with value error")
     # print("Error message mereketengue")
     # sys.exit(1)
