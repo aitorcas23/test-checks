7e1dee7a746d2ba06791646b12bbe01ba898f274
aitorcas <aitorcas23@gmail.com>
2024-03-23 17:11:53 +0100

    Change two files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Tests for check runs
+
+Change this file
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 if __name__ == "__main__":
     # print("::warning file=main.py,line=5,title=Warning title::Send warning from python script")
     # raise ValueError("Error with value error")
+
+    # Change this file
     print("Hello github action")
