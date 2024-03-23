a576d0c4130ec62cef27ccb7db5190e898d55dda
aitorcas <aitorcas23@gmail.com>
2024-03-23 19:20:57 +0100

    ya va?

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ if __name__ == "__main__":
     # print("::warning file=main.py,line=5,title=Warning title::Send warning from python script")
     # raise ValueError("Error with value error")
 
-    # now draw her giving birth
+    # Y si hago otro cambio aqui?
     print("Hello github action")
